b07b40fef21ffe6699117c2a380305644a2cc24c
Sreehari <sreeharimangalasseri528@gmail.com>
2024-08-16 11:15:12 +0530

    react lesson 2

---
+++ first-react-project/src/App.jsx
@@ -0,0 +1,16 @@
+// import Header from "./header.jsx"
+// import Footer from "./footer.jsx"
+// import Food from "./food.jsx"
+import Card from "./card.jsx"
+
+function App() {
+
+  return(
+    <>
+    <Card></Card>
+    <Card></Card>
+    </>
+  )
+}
+
+export default App
---
+++ first-react-project/src/card.jsx
@@ -0,0 +1,14 @@
+
+// import profilePic from './assets/straw.jpeg'
+
+function Card(){
+    return (
+        <div className='card'>
+            <img classname='card-image' src="https://via.placeholder.com/150" alt="profilePic" />
+            <h2 className='card-title'>Rambo9320</h2>
+            <p className='card-text'>Im a student and im learning mernstack</p>
+        </div>
+    )
+}
+
+export default Card
