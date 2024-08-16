77b89152f88c7a9c4fe28480ac212906a7450620
Sreehari <sreeharimangalasseri528@gmail.com>
2024-08-16 14:07:17 +0530

    lesson 4

--- first-react-project/src/App.jsx
+++ first-react-project/src/App.jsx
@@ -2,12 +2,19 @@
 // import Footer from "./footer.jsx"
 // import Food from "./food.jsx"
 // import Card from "./card.jsx"
-import Button from "./button.jsx"
+// import Button from "./button.jsx"
+import Student from "./student.jsx"
 
 function App() {
 
   return(
-    <Button/>
+    <>
+    <Student name='SpongeBob' age={25} isStudent={true}></Student>
+    <Student name='Patrick' age={28} isStudent={false}></Student>
+    <Student name='Squidward' age={33} isStudent={false}></Student>
+    <Student name='Sandy' age={28} isStudent={true}></Student>
+    <Student name = 'larry'></Student>
+    </>
   )
 }
 
