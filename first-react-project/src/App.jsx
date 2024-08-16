43851f069e9bb13879944f9c5590df9f8771c080
Sreehari <sreeharimangalasseri528@gmail.com>
2024-08-16 18:32:51 +0530

    useState hook

--- first-react-project/src/App.jsx
+++ first-react-project/src/App.jsx
@@ -3,19 +3,18 @@
 // import Food from "./food.jsx"
 // import Card from "./card.jsx"
 // import Button from "./button.jsx"
-import Student from "./student.jsx"
+// import Student from "./student.jsx"
+// import UserGreeting from "./userGreeting.jsx"
+// import List from "./list.jsx";
+// import Button from "./button.jsx";
+// import ProfilePicture from "./profilePicjure.jsx";
+import MyComponent from "./myComponent.jsx";
+import Counter from "./counter.jsx";
 
 function App() {
 
-  return(
-    <>
-    <Student name='SpongeBob' age={25} isStudent={true}></Student>
-    <Student name='Patrick' age={28} isStudent={false}></Student>
-    <Student name='Squidward' age={33} isStudent={false}></Student>
-    <Student name='Sandy' age={28} isStudent={true}></Student>
-    <Student name = 'larry'></Student>
-    </>
-  )
+  return( <Counter /> );
+
 }
 
-export default App
+export default App;
---
+++ first-react-project/src/myComponent.jsx
@@ -0,0 +1,32 @@
+
+import React, {useState} from 'react'
+
+function MyComponent(){
+
+    const [name, setName] = useState('batman');
+    const [age,setAge] = useState(0)
+    const [isEmployed, setIsEmployed] = useState(false)
+
+    const updateName = () =>{
+        setName('nightwing')
+        
+    }
+    const incrementAge = () =>{
+        setAge(age+1)
+    }
+    const togglestatus = () =>{
+        setIsEmployed(!isEmployed)
+    }
+
+    return(<div>
+        <p>Name: {name}</p>
+        <button onClick={updateName}> Set Name</button>
+
+        <p>Age: {age}</p>
+        <button onClick={incrementAge}> Increment Age</button>
+
+        <p>is Employed: {isEmployed?'Yes':'No'}</p>
+        <button onClick={togglestatus}> Toggle</button>
+    </div>)
+}
+export default MyComponent
